1ae21825acd6393b82f799fe9ed8faefc3575a76
Sahil Chug <sahil@p3infotech.in>
2013-01-22 09:10:58 +0530

    added arg.rb

---
+++ arg.rb
@@ -0,0 +1,7 @@
+first, second, third = ARGV
+
+puts "The script is called: #{$0}"
+puts "your first variable is : #{first}"
+
+puts "your second variable is : #{second}"
+puts "your third variable is : #{third}"
