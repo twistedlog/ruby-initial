ddd9a346a39e264445bc509c3d4916e2829572a6
Sahil Chug <sahil@p3infotech.in>
2013-01-22 08:50:47 +0530

    formatter behaviour "give less arguments it gives error" give more arguments it ignores extra argument

--- formatter.rb
+++ formatter.rb
@@ -3,3 +3,4 @@ formatter = "%s %s %s %s"
 puts formatter % [1, 2, 3, 4]
 puts formatter % ["one", "two", "three", "four"]
 puts formatter % [true, false, false, true]
+puts formatter % [formatter, formatter, formatter, formatter, formatter]
